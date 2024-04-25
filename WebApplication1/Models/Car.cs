1611910b9c39f69d5248e787df37573fba3bcddc
Hayotilla Tursunboev <coder0227@gmail.com>
2024-04-25 17:28:17 +0300

    Add project files.

---
+++ WebApplication1/Models/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string ?model { get; set; }
+        public string ?Brand { get; set; }
+    }
+}
